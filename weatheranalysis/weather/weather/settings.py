b2a7bcb1ce13facc8c9f1bfc67ef66c63773d9ea
nrj00 <nrj5034@gmail.com>
2024-08-27 02:44:23 +0530

    deployment (os.environ)

--- weatheranalysis/weather/weather/settings.py
+++ weatheranalysis/weather/weather/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import dj_database_url
 from pathlib import Path
 import os #for static file
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather.settings')
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
